251dcecbb883472263dbb71b2e27931eb97296b9
Gauthier Jeannenot <jeannenot.gauthier@gmail.com>
2022-11-04 18:09:33 +0100

    checkCollision partie 1

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import { createStage } from "../gameHelpers";
+import { createStage, checkCollision } from "../gameHelpers";
 
 //Styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
@@ -20,25 +20,39 @@ const Tetris = () => {
 
 
     const [player, updatePlayerPos, resetPlayer] = usePlayer()
-    const [stage, setStage] = useStage(player)
+    const [stage, setStage] = useStage(player, resetPlayer)
 
 
 
-    console.log('re-render')
 
-    const movePlayer = dir => {
-        updatePlayerPos({ x: dir, y: 0})
+
+    const movePlayer = (dir) => {
+        if (!checkCollision (player, stage, {x : dir, y : 0})) {
+            updatePlayerPos({ x: dir, y: 0})
+        } 
     }
 
     const startGame = () => {
         //reset everything
         setStage(createStage())
         resetPlayer()
+        setGameOver(false)
         
     }
 
     const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false})
+        if (!checkCollision (player, stage, {x : 0, y : 1})) {
+            updatePlayerPos({ x: 0, y: 1, collided: false})
+        } else {
+            if (player.pos.y < 1) {
+                console.log("game over")
+                setGameOver(true)
+                setDropTime(null)
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true})
+
+        }
+        
     }
 
     const dropPlayer = () => {
@@ -49,9 +63,7 @@ const Tetris = () => {
        
         if (!gameOver) {
             if (keyCode === 37) {
-                console.log(player)
                 movePlayer(-1)
-                console.log(player)
             } else if (keyCode === 39) {
                 movePlayer(1)
             } else if (keyCode === 40) {
